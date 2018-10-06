98a037290b9638812b04f2d572e2e4102d97ad4d
avellent <40453042+avellent@users.noreply.github.com>
2018-10-06 14:37:39 +0200

    Add files via upload

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
-# SimpleTestSkill
-Simple test skill
+This skill will read back someone who died that day in history. It shows the use
+of the API that Mycroft offers and the use of a web API called Today In History.
+
+Coded with the Skill Template
+
+Author Steven Ohrdorf
---
+++ __init__.py
@@ -0,0 +1,54 @@
+from os.path import dirname
+
+from adapt.intent import IntentBuilder
+from mycroft.skills.core import MycroftSkill
+from mycroft.util.log import getLogger
+import requests
+
+__author__ = 'Steven'
+
+# Logger: used for debug lines, like "LOGGER.debug(xyz)". These
+# statements will show up in the command line when running Mycroft.
+LOGGER = getLogger(__name__)
+
+# The logic of each skill is contained within its own class, which inherits
+# base methods from the MycroftSkill class with the syntax you can see below:
+# "class ____Skill(MycroftSkill)"
+class SimpleSkill(MycroftSkill):
+
+    # The constructor of the skill, which calls MycroftSkill's constructor
+    def __init__(self):
+        super(SimpleSkill, self).__init__(name="SimpleSkill")
+
+    # This method loads the files needed for the skill's functioning, and
+    # creates and registers each intent that the skill uses
+    def initialize(self):
+        self.load_data_files(dirname(__file__))
+
+        random_event_intent = IntentBuilder("RandomEventIntent").\
+            require("RandomEventKeyword").build()
+        self.register_intent(random_event_intent, self.handle_random_event_intent)
+
+
+
+    # The "handle_xxxx_intent" functions define Mycroft's behavior when
+    # each of the skill's intents is triggered: in this case, he simply
+    # speaks a response. Note that the "speak_dialog" method doesn't
+    # actually speak the text it's passed--instead, that text is the filename
+    # of a file in the dialog folder, and Mycroft speaks its contents when
+    # the method is called.
+    def handle_random_event_intent(self, message):
+        self.speak("it is a test for the research paper")
+
+
+    # The "stop" method defines what Mycroft does when told to stop during
+    # the skill's execution. In this case, since the skill's functionality
+    # is extremely simple, the method just contains the keyword "pass", which
+    # does nothing.
+    def stop(self):
+        pass
+
+# The "create_skill()" method is used to create an instance of the skill.
+# Note that it's outside the class itself.
+def create_skill():
+    return SimpleSkill()
